634aff687a44281e8ef4dc31bfaab9b3bdbca836
Michael Stevens <mls0.5mv2@gmail.com>
2013-04-24 22:31:07 -0400

    improvement to ranSelect function

--- src/Math.scala
+++ src/Math.scala
@@ -4,14 +4,10 @@ object Math {
   def dist(p1: Array[Double], p2: Array[Double]) =
     math sqrt (for (i <- List.range(0, p1.length)) yield math.pow(p1(i) - p2(i), 2)).sum
 
-  def ranSelect[T](list: List[T], i: Int): List[T] =
-    i match {
-      case _ if list == Nil => Nil
-      case 0 => Nil
-      case x =>
-        val v = list(new Random().nextInt(list.size))
-        v :: ranSelect(list diff List(v), i - 1)
-    }
+  def ranSelect[T](list: List[T], i: Int): List[T] = {
+    lazy val new_list = Random shuffle list
+    new_list take i
+  }
 
   def ave(list: List[Array[Double]]): Array[Double] =
     list.reduceLeft((x: Array[Double], y: Array[Double]) =>
